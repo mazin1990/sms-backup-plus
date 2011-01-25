53c997d93da2240a865209c813423aa160f40161
Markus Doits <markus.doits@googlemail.com>
2011-01-25 22:09:17 +0100

    Remove APG's sources and add jar containing classes

--- src/org/thialfihar/android/apg/utils/ApgConInterface.java
+++
@@ -1,9 +0,0 @@
-package org.thialfihar.android.apg.utils;
-
-import android.os.Bundle;
-
-public interface ApgConInterface {
-    public static interface OnCallFinishListener {
-        public abstract void onCallFinish(Bundle result);
-    }
-}
